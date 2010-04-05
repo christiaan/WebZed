15e04636c9711734a5936168334b5b052435357c
Christiaan Baartse <anotherhero@gmail.com>
2010-04-05 20:18:58 +0200

    Calling of callback covered with tests

--- tests/IntervalTest.js
+++ tests/IntervalTest.js
@@ -1,9 +1,12 @@
 module("Interval", {
 	setup: function() {
-		this.callback = function() {
-			this.args = arguments;
+		var callback = function() {
+			callback.context = this;
+			callback.args = arguments;
 		};
-		this.obj = new Interval(this.callback, 1000 / 30);
+	
+		this.callback = callback; 
+		this.obj = new Interval(this.callback, 100);
 	}
 });
 
@@ -30,7 +33,7 @@ test("throws an exception when an invalid delay is given", function() {
 test("type error thrown if elapsed is set but no Number", function() {
 	var thrown = false;
 	try {
-		new Interval(this.callback, 1000/30, "tweehonderd");
+		new Interval(this.callback, 100, "tweehonderd");
 	} catch (e) {
 		thrown = true;
 	}
@@ -39,11 +42,6 @@ test("type error thrown if elapsed is set but no Number", function() {
 
 test("Interval is not started when just created", function() {
 	ok(!this.obj.started);
-});
-
-test("Start the Interval", function() {
-	this.obj.start();
-	ok(this.obj.started);
 });
 
 test("Interval throw exception when stopping and not started", function() {
@@ -60,4 +58,29 @@ test("Interval can be stopped when it has been started", function() {
 	this.obj.start();
 	this.obj.stop();
 	ok(!this.obj.started);
+});
+
+test("callback is called within the context of the interval", function() {
+	this.obj.update(0);
+	same(this.callback.context, this.obj);
+});
+
+test("callback is called with incrementing delay", function() {
+	this.obj.elapsed = 0;
+	this.obj.update(0);
+	equals(this.callback.args[0], 100);
+});
+
+test("elapsed gets incremented on each update call", function() {
+	this.obj.elapsed = 0;
+	this.obj.update(0);
+	this.obj.update(0);
+	equals(this.callback.args[0], 200);
+});
+
+test("elapsed gets incremented by delay and overdue", function() {
+	this.obj.elapsed = 0;
+	this.obj.update(10);
+	this.obj.update(-5);
+	equals(this.obj.elapsed, 205);
 });
